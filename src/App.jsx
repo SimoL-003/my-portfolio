c8e4188fb18d1fbfc998f393df370b0bd6a84372
SimoL-003 <simoneleuzzi2003@gmail.com>
2025-12-02 18:32:46 +0100

    feat: create pages and set up routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contacts from "./pages/Contacts";
+import NotFound from "./pages/NotFound";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path={"about-me"} element={<About />} />
+          <Route path={"projects"} element={<Projects />} />
+          <Route path={"contacts"} element={<Contacts />} />
+          <Route path={"*"} element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
