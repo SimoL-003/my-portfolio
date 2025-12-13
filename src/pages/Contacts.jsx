56475bd1676191b75c8c6fc64562a08d852338dd
SimoL-003 <simoneleuzzi2003@gmail.com>
2025-12-13 12:13:44 +0100

    feat(contacts): setup main section

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,3 +1,16 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { icons } from "../assets/icons/icons";
+
 export default function Contacts() {
-  return <h1>Contacts</h1>;
+  return (
+    <section className="min-h-[calc(100vh-310.5px)] flex">
+      <div className="container flex">
+        <div className="w-1/2">
+          <FontAwesomeIcon icon={icons.handsShaking} />
+          <h1>Entriamo in contatto</h1>
+        </div>
+        <div className="w-1/2">form</div>
+      </div>
+    </section>
+  );
 }
