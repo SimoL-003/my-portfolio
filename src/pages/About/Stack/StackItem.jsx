10039eb5cdc03352b01048a34a30a2868d230104
SimoL-003 <simoneleuzzi2003@gmail.com>
2026-01-18 10:33:42 +0100

    ui(about): improve UI in stack section

--- src/pages/About/Stack/StackItem.jsx
+++ src/pages/About/Stack/StackItem.jsx
@@ -1,11 +1,7 @@
 export default function StackItem({ title, icon }) {
   return (
-    <li className="flex flex-col items-center transition-all hover:scale-105">
-      <img
-        src={icon}
-        alt={title}
-        className="w-10 max-h-10 mb-3 drop-shadow-white/20 drop-shadow-2xl"
-      />
+    <li className="flex flex-col items-center transition-all">
+      <img src={icon} alt={title} className="w-10 max-h-10 mb-3" />
       <span className="tracking-wider font-semibold text-sm">{title}</span>
     </li>
   );
